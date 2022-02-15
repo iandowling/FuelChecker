d1f56f372d24ee4ff0e67b4747f6e9536901617d
iandowling <idowling88@outlook.com>
2022-02-15 21:01:35 +0000

    First commit

---
+++ src/main/java/com/io/fuelchecker/FuelCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.io.fuelchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelCheckerApplication.class, args);
+	}
+
+}
